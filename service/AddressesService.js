997438c81b41ac18d227ca3cea8d3ea32f8def20
Katrina Behnken <katrina.behnken@gmail.com>
2020-03-28 12:33:47 -0700

    remove unused code

--- service/AddressesService.js
+++ service/AddressesService.js
@@ -1,6 +1,5 @@
 'use strict';
 let respondWithCode = require('../utils/writer').respondWithCode;
-let isEmpty = require('../utils/utils').isEmpty;
 
 
 /*
@@ -9,14 +8,13 @@ let isEmpty = require('../utils/utils').isEmpty;
  * returns schema/ArrayOfAddresses
  **/
 exports.getAddresses = function() {
-  console.log(db)
   return new Promise(async function(resolve, reject) {
     await db.get_addresses()
     .then(addresses => {
       resolve(addresses)
     })
     .catch(err => {
-      reject(utils.respondWithCode(400, err));
+      reject(utils.respondWithCode(500, err));
     });
   })
 }
@@ -32,13 +30,11 @@ exports.getAddressById = function(id) {
     await db.get_address_by_id(id)
     .then(address => {
        if (address.length === 0){
-         console.log("404")
-          resolve(respondWithCode(404, "Service address not found"))
+        resolve(respondWithCode(404, "Service address not found"))
        }
        resolve(address)
     })
     .catch(err => {
-      console.log(err)
       reject(respondWithCode(500, err));
     });
   })
