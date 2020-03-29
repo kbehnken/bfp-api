f8b9ad965823d17d23cec743d32daef8bc3d369e
Tim Behnken <tbehnken@llnw.com>
2020-03-28 18:12:25 -0700

    Updated .gitignore. Added massive.js to package.json. Added 2 controllers

---
+++ controllers/ServiceCalls.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var ServiceCalls = require('../service/ServiceCallsService');
+
+module.exports.addServiceCall = function addServiceCall (req, res, next, body) {
+  ServiceCalls.addServiceCall(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getServiceCallById = function getServiceCallById (req, res, next, id) {
+  ServiceCalls.getServiceCallById(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.getServiceCalls = function getServiceCalls (req, res, next) {
+  ServiceCalls.getServiceCalls()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
