1ba4e0f76f9d85e7e27fc0f7bd6a5f7a2ab71982
Katrina Behnken <katrina.behnken@gmail.com>
2020-04-08 13:24:05 -0700

    Added aliases and join clauses

--- db/get_addresses.sql
+++ db/get_addresses.sql
@@ -1,1 +1,8 @@
-SELECT * FROM service_addresses;
+SELECT sa.id AS id, sa.street_address AS "streetAddress", sa.city, sa.state, sa.postal_code AS "postalCode", sa.user_id AS "userId", sa.map_url AS "mapUrl", sa.photo_url AS "photoUrl", sa.latitude, sa.longitude, sa.customer_id AS "customerId", sa.vendor_id AS "vendorId", a.id AS "assetId", a.make AS "assetMake", a.model AS "assetModel", a.category AS "assetCategory"
+FROM service_addresses AS sa
+LEFT JOIN users AS u
+ON u.id = sa.user_id
+LEFT JOIN assets_to_service_addresses AS atsa
+ON atsa.service_address_id = sa.id
+LEFT JOIN assets as a
+ON atsa.asset_id = a.id;
