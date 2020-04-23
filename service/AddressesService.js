56ea3e561c2bdb975e5675ef37975e07133e55fd
Katrina Behnken <katrina.behnken@gmail.com>
2020-04-23 10:45:58 -0700

    Added addAddress, addAssetToAddress, and removeAssetFromAddress functions

--- service/AddressesService.js
+++ service/AddressesService.js
@@ -1,25 +1,43 @@
 'use strict';
+
 let respondWithCode = require('../utils/writer').respondWithCode;
 
 /**
- * Adds a single address
- **/
+  * Adds a single address
+**/
 exports.addAddress = function(body) {
   return new Promise(async function(resolve, reject) {
-    const newAddress = Address.build(body);
-    await newAddress.save()
-    .then(address => {
-      resolve(address)
+    let newAddress = Address.build(body);
+    let assets = body.assets || [];
+
+    // Create the new address and persist to DB
+    newAddress = await newAddress.save()
+    .then(newAddress => {
+      return(newAddress)
     })
     .catch(err => {
       reject(respondWithCode(500, err));
     });
+    
+    // Add an asset association for every id in assets
+    for(let i = 0; i < addresses.length; i++) {
+      await newAddress.addAddress(addresses[i])
+      .then()
+      .catch(err => {
+        reject(respondWithCode(500, err));
+      });
+    }
+
+    // Update the address with the assets data and resolve
+    resolve(newAddress.reload({
+      include: [ Asset ]
+    }))
   });
 }
-/*
- * Returns a list of all service addresses - An array of objects
- **/
 
+/**
+  * Returns a list of all addresses - An array of objects
+**/
 exports.getAddresses = function() {
   return new Promise(async function(resolve, reject) {
     await Address.findAll()
@@ -33,25 +51,65 @@ exports.getAddresses = function() {
 }
 
 /**
- * Returns a single service address
+  * Returns a single address
 **/
 exports.getAddressById = function(addressId) {
-  console.log(addressId)
   return new Promise(async function(resolve, reject) {
     await Address.findByPk(addressId, {
-        include: [{
-            model: ServiceCall,
-            include: [ Service ]
-        }]
+      include: [ Asset ]
     })
     .then(address => {
-       if (address.length === 0){
-        resolve(respondWithCode(404, "Service address not found"))
-       }
-       resolve(address)
+       if(!address) {
+        reject(respondWithCode(404, "Address not found"))
+      }
+      resolve(address)
     })
     .catch(err => {
       reject(respondWithCode(500, err));
     });
-  })
+  });
+}
+
+/**
+ * Adds a single asset to an address
+**/
+exports.addServiceToServiceCall = function(serviceCallId, serviceId) {
+  return new Promise(async function(resolve, reject) {
+    
+    // Fetch the address
+    await Address.findByPk(addressId)
+    .then(address => address)
+    .catch(err => {
+        reject(respondWithCode(500, err));
+    });
+            
+    // Add the asset
+    await Address.addAsset(assetId)
+    .then(address => {
+      resolve(address.reload({
+          include: [ Asset ]
+      }))
+    })
+    .catch(err => {
+        reject(respondWithCode(500, err));
+    });
+  });
+}
+
+/**
+ * Removes an asset from an address
+**/
+exports.removeAssetFromAddress = function(addressId, assetId) {
+  return new Promise(async function(resolve, reject) {
+    await AssetsToAddresses.destroy({
+      where: {
+          addressId: addressId,
+          assetId: assetId
+      }
+    })
+    .then(resolve(await Address.findByPk(addressId)))
+    .catch(err => {
+        reject(respondWithCode(500, err));
+    });
+  });
 }
