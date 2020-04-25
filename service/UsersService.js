8507238d7d26264813b8f342be31d9f380a1cb7c
Katrina Behnken <katrina.behnken@gmail.com>
2020-04-25 16:24:31 -0700

    Modified getUsers endpoint to work with Sequelize

--- service/UsersService.js
+++ service/UsersService.js
@@ -1,36 +1,46 @@
 'use strict';
 
+let respondWithCode = require('../utils/writer').respondWithCode;
 
 /**
  * Returns a list of all users
- *
- * returns Users
- **/
-exports.getUsers = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "isAdmin" : true,
-  "userRole" : "employee",
-  "firstName" : "firstName",
-  "lastName" : "lastName",
-  "phoneNumber" : "phoneNumber",
-  "emailAddress" : "emailAddress",
-  "hash" : "hash"
-}, {
-  "isAdmin" : true,
-  "userRole" : "employee",
-  "firstName" : "firstName",
-  "lastName" : "lastName",
-  "phoneNumber" : "phoneNumber",
-  "emailAddress" : "emailAddress",
-  "hash" : "hash"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
+**/
+// exports.getUsers = function() {
+//   return new Promise(function(resolve, reject) {
+//     var examples = {};
+//     examples['application/json'] = [ {
+//   "isAdmin" : true,
+//   "userRole" : "employee",
+//   "firstName" : "firstName",
+//   "lastName" : "lastName",
+//   "phoneNumber" : "phoneNumber",
+//   "emailAddress" : "emailAddress",
+//   "hash" : "hash"
+// }, {
+//   "isAdmin" : true,
+//   "userRole" : "employee",
+//   "firstName" : "firstName",
+//   "lastName" : "lastName",
+//   "phoneNumber" : "phoneNumber",
+//   "emailAddress" : "emailAddress",
+//   "hash" : "hash"
+// } ];
+//     if (Object.keys(examples).length > 0) {
+//       resolve(examples[Object.keys(examples)[0]]);
+//     } else {
+//       resolve();
+//     }
+//   });
+// }
 
+exports.getUsers = function() {
+  return new Promise(async function(resolve, reject) {
+    await User.findAll()
+    .then(users => {
+      resolve(users)
+    })
+    .catch(err => {
+      reject(utils.respondWithCode(500, err));
+    });
+  })
+}
