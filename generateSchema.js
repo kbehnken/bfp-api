76b2ffc117459ae1d61435f041260a751f61a5e2
Tim Behnken <tbehnken@llnw.com>
2020-05-14 09:13:16 -0700

    update generateSchema.js to accept a command-line argument

--- generateSchema.js
+++ generateSchema.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize');
 const sjs = require('sequelize-json-schema');
 require('dotenv').config;
 
-model = process.argv[1]
+model = process.argv[2]
+console.log(model);
 const { DB, DB_USER, DB_PASSWORD } = process.env;
 global.sequelize = new Sequelize('bfp_api', 'bfp_api', 'bfp_api', {
   dialect: 'mysql'
 });
 require('./models/models.js');
-console.log(sjs.getModelSchema(Service));
-
+console.log(sjs.getModelSchema(global[model]));
